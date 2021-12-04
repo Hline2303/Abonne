2d2b834fdc572854586916770a673a3218b1f947
Solène Médina <solene.medina.pro@gmailcom>
2021-12-04 11:31:15 +0100

    Dynamisation de l'icône et du bouton

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('connecté !');
+// console.log("connecté !");
+
+const icone = document.querySelector("i");
+// console.log(icone);
+
+const btnButton = document.querySelector(".btn");
+console.log(btnButton);
+// Transformation de l'icône au clic
+icone.addEventListener("click", function () {
+  console.log("icone cliquée");
+  icone.classList.toggle("fa-smile-wink");
+  icone.classList.toggle("happy");
+});
+
+//  Je modifie le bouton au clic
+btnButton.addEventListener("click", function () {
+  console.log("bouton cliqué !");
+  btnButton.classList.toggle("abonné");
+  if (btnButton.innerText === "Abonné") {
+    btnButton.innerText = "Abonnez-vous";
+  } else {
+    console.log("else");
+    btnButton.innerText = "Abonné";
+  }
+});
